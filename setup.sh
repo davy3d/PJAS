770f548dadadffdae4a511c18a9fb55679d04b7c
David Oven <doven@seton-school.com>
2025-10-17 21:58:10 -0400

    adding setup.sh and requirements.txt

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#sudo apt-get update -y
+#sudo apt-get install python3-pip python3-venv
+
+cd ..
+
+python -m venv ~/PJAS
+
+~/PJAS/bin/python -m pip install -r ~/PJAS/requirements.txt
+
+cd PJAS
