cef98858b974b3d3a874803b13789df34be32f7a
David Oven <doven@seton-school.com>
2025-10-26 17:07:52 -0400

    updating pic taker for creating harr

--- rasipi_picturetaker.py
+++ rasipi_picturetaker.py
@@ -1,1 +1,30 @@
-import this
+import cv2
+import datetime
+from time import sleep
+
+
+while True:
+    
+    # Access the default camera
+    cap = cv2.VideoCapture(0)
+    
+    # Check if camera opened successfully
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        
+    else:
+        # Capture a single frame
+        ret, frame = cap.read()
+        
+        timestamp = datetime.datetime.now().isoformat()
+
+        if ret:
+            # Save the captured frame
+            cv2.imwrite(f"image_{timestamp}.jpg", frame)
+            print("Image saved as captured_image.jpg")
+        else:
+            print("Failed to capture image.")
+            
+        # Release the camera
+        cap.release()
+        sleep(30)
